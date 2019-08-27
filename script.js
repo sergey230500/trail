d5dd35a152fe8b7ae0a63e438f605d5e8ea84581
sergey230500 <sergey230500@mail.ru>
2019-08-27 22:31:47 +0300

    Оптимизация

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function init(){
 	const length = 40;
 	const lifeTime = 300;
+	let trail = [];
 	document.body.addEventListener("mousemove", ({x, y}) => {
 		let div = document.createElement("div");
 		div.classList.add("trail");
@@ -9,9 +10,17 @@ function init(){
 		document.body.appendChild(div);
 		div.clientWidth;
 		div.style.opacity  = 0;
-		setTimeout(() => {
-			document.body.removeChild(div);
-		}, lifeTime);
+		if (!trail.length) setTimeout(deleteOldestDiv, lifeTime);
+		trail.push([div, Date.now() + lifeTime]);
 	});
+	
+	function deleteOldestDiv(){
+		let [div, time] = trail.shift();
+		document.body.removeChild(div);
+		if (trail.length){
+			time = trail[0][1];
+			setTimeout(deleteOldestDiv, Math.max(time - Date.now(),1));
+		}
+	}
 }
 init();
